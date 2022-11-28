41ddacf1af911b80b5446fe39270099b21f6b14f
zuluarnold0 <zuluarnold0@gmail.com>
2022-11-28 23:33:34 +0200

    count up implemented on home total component

--- src/component/HomeTotal/HomeTotal.component.js
+++ src/component/HomeTotal/HomeTotal.component.js
@@ -1,11 +1,13 @@
-
-import { PureComponent } from 'react';
+import { useState } from 'react';
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
 
 import './HomeTotal.style.scss';
 
-class HomeTotal extends PureComponent {
+const HomeTotal = () => {
+    const [ counterOn, setCounterOn ] = useState(false);
 
-    totalsList = [
+    const totalsList = [
         {
             id: 'new zxdxcfcvstudents',
             count: '100',
@@ -28,7 +30,7 @@ class HomeTotal extends PureComponent {
         },
     ];
 
-    renderImage() {
+    const renderImage = () => {
         const url = `${process.env.PUBLIC_URL}/assets/images/home-total/home-total.jpg`;
 
         return (
@@ -42,11 +44,11 @@ class HomeTotal extends PureComponent {
         );
     }
     
-    renderListItem({ id, count, descr }) {
+    const renderListItem = ({ id, count, descr }) => {
         return (
             <div className='HomeTotal-ListItem' key={id}>
                 <div className='HomeTotal-CountContainer'>
-                    <p className='HomeTotal-Count'>{count}</p>
+                    { counterOn && <CountUp className='HomeTotal-Count' start={0} end={count} duration={2} /> }
                 </div>
                 <div className='HomeTotal-DescrContainer'>
                     <p className='HomeTotal-DescrValue'>{descr}</p>
@@ -56,30 +58,132 @@ class HomeTotal extends PureComponent {
         );
     }
 
-    renderTotals() {
-        const { totalsList } = this;
-
+    const renderTotals = () => {
         return (
             <div className='HomeTotal-Totals Split'>
-                { totalsList.map(this.renderListItem)}
+                { totalsList.map(renderListItem)}
             </div>
         )
     }
 
-    renderComponents() {
+    const renderComponents = () => {
         return (
-          <section className='HomeTotal'>
-            { this.renderImage()}
-            <div className='HomeTotal-Container ContainerWrapper'>
-                { this.renderTotals() }
-            </div>
-          </section>
+            <ScrollTrigger
+                onEnter={() => setCounterOn(true)}
+                onExit={() => setCounterOn(false)}
+            >
+                <section className='HomeTotal'>
+                    { renderImage()}
+                    <div className='HomeTotal-Container ContainerWrapper'>
+                        { renderTotals() }
+                    </div>
+                </section>
+            </ScrollTrigger>
         );
     }
 
-    render() {
-        return this.renderComponents();
-    }
+    return renderComponents();
 }
 
 export default HomeTotal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { PureComponent } from 'react';
+
+// import './HomeTotal.style.scss';
+
+// class HomeTotal extends PureComponent {
+
+//     totalsList = [
+//         {
+//             id: 'new zxdxcfcvstudents',
+//             count: '100',
+//             descr: 'new students',
+//         },
+//         {
+//             id: 'newbnjnj students',
+//             count: '60',
+//             descr: 'new students',
+//         },
+//         {
+//             id: 'newbhbhbh students',
+//             count: '187',
+//             descr: 'new students',
+//         },
+//         {
+//             id: 'new studentshuhu',
+//             count: '342',
+//             descr: 'new students',
+//         },
+//     ];
+
+//     renderImage() {
+//         const url = `${process.env.PUBLIC_URL}/assets/images/home-total/home-total.jpg`;
+
+//         return (
+//             <div className='HomeTotal-Image'>
+//                 <img
+//                     className='HomeTotal-Img'
+//                     src={url} 
+//                     alt='hospital items'
+//                 />
+//             </div>
+//         );
+//     }
+    
+//     renderListItem({ id, count, descr }) {
+//         return (
+//             <div className='HomeTotal-ListItem' key={id}>
+//                 <div className='HomeTotal-CountContainer'>
+//                     <p className='HomeTotal-Count'>{count}</p>
+//                 </div>
+//                 <div className='HomeTotal-DescrContainer'>
+//                     <p className='HomeTotal-DescrValue'>{descr}</p>
+//                     <p className='HomeTotal-Descr'>{'Every Year'}</p>
+//                 </div>
+//             </div>
+//         );
+//     }
+
+//     renderTotals() {
+//         const { totalsList } = this;
+
+//         return (
+//             <div className='HomeTotal-Totals Split'>
+//                 { totalsList.map(this.renderListItem)}
+//             </div>
+//         )
+//     }
+
+//     renderComponents() {
+//         return (
+//           <section className='HomeTotal'>
+//             { this.renderImage()}
+//             <div className='HomeTotal-Container ContainerWrapper'>
+//                 { this.renderTotals() }
+//             </div>
+//           </section>
+//         );
+//     }
+
+//     render() {
+//         return this.renderComponents();
+//     }
+// }
+
+// export default HomeTotal;
